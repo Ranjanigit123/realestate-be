e6141deae9fdf8ee86f2297ec2f8a7027cff6802
Ranjanigit123 <ranjanirithu206@gmail.com>
2024-12-05 20:11:14 +0530

    Initial commit

--- server.js
+++ server.js
@@ -9,7 +9,11 @@ const propertyRoutes = require("./routes/propertyRoutes");
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
+//app.use(cors());
+app.use(cors({
+    origin: "https://realestate34.netlify.app",
+    methods: "GET,POST,PUT,DELETE",
+}));
 app.use(express.json());
 app.use(bodyParser.json());
 
